4c2257852be9d037ea422f4fe3b4288fbee8955a
Park Jae Yun <wodbs720@naver.com>
2022-08-30 23:27:42 +0900

    learn data type

--- KotlinBasics/app/src/main/java/com/jaeyunpark/kotlinbasics/Basics.kt
+++ KotlinBasics/app/src/main/java/com/jaeyunpark/kotlinbasics/Basics.kt
@@ -1,7 +1,31 @@
 package com.jaeyunpark.kotlinbasics
 
 fun main(){
+    // immutable variable
+    // TODO: Add new functionality
+
+    /*
+    this
+    is
+    comment
+     */
+
+    // type string
     var myName = "Denis"
+    // type int 32 bit
+    // type inference finds out the type from context
+    var myAge = 31
+    var isSunny: Boolean = true
+    isSunny = false
+
+    // Characters
+    var letterChar = 'A'
+
+    // Strings
+    val myStr = "Hello World"
+    var firstCharInStr = myStr[0]
+    var lastCharInStr = myStr[myStr.length -1]
+
     myName = "Claudia"
-    print("Hello " + myName)
+    print("Last Character " + lastCharInStr)
 }
