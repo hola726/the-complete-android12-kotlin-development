6994690d965735da49b9ea19df20c2bb9a23840d
Park Jae Yun <wodbs720@naver.com>
2022-09-01 23:06:41 +0900

    learn when

--- KotlinBasics/app/src/main/java/com/jaeyunpark/kotlinbasics/Basics.kt
+++ KotlinBasics/app/src/main/java/com/jaeyunpark/kotlinbasics/Basics.kt
@@ -96,5 +96,39 @@ fun main(){
     if(isRainy)
         println("It's rainy")
 
+    var season = 3
+    when(season){
+        1 -> println("Spring")
+        2 -> println("Summer")
+        3 -> {
+            println("Fall")
+            println("Autumn")
+        }
+        4 -> println("Winter")
+        else -> println("Invalid Season")
+    }
+    var month = 3
+    when(month){
+        in 3..5 -> println("Spring")
+        in 6..8 -> println("Summer")
+        in 9..11 -> println("Fall")
+        in 12..2 -> println("Winter")
+        else -> println("Invalid Season")
+    }
+
+    when(age){
+        in 21..150 -> println("now you may drink in the US")
+        in 18..20 -> println("you may vote now")
+        16,17 -> println("you may drive now")
+        else -> println("you're too young")
+    }
+
+    var x : Any = 13.37f
+    when(x){
+        is Int -> println("$x is an Int")
+        !is Double -> println("$x is not a Double")
+        is String -> println("$x is a String")
+        else -> println("$x is none of the above")
+    }
 
 }
