f997b11985688d1d051dc30150e2c464976ccb9d
Park Jae Yun <wodbs720@naver.com>
2022-09-01 23:59:57 +0900

    learn for

--- KotlinBasics/app/src/main/java/com/jaeyunpark/kotlinbasics/Basics.kt
+++ KotlinBasics/app/src/main/java/com/jaeyunpark/kotlinbasics/Basics.kt
@@ -160,4 +160,28 @@ fun main(){
             println("it's comfy now")
         }
     }
+
+    for(num in 1..10){
+        print("$num ")
+    }
+    println()
+    for(i in 1 until 10){
+        print("$i ")
+    }
+
+    println()
+    for(i in 10 downTo 1 step 2){
+        print("$i ")
+    }
+    println()
+
+    for(i in 1 until 20){
+
+        if(i/2 == 5){
+            continue
+        }
+        print("$i ")
+
+    }
+    print("Done with the loop")
 }
