49de0f497e2e0797e93afe0366dfa13271f0085c
Park Jae Yun <wodbs720@naver.com>
2022-09-06 23:23:42 +0900

    learn data class

--- KotlinBasics/app/src/main/java/com/jaeyunpark/kotlinbasics/Basics.kt
+++ KotlinBasics/app/src/main/java/com/jaeyunpark/kotlinbasics/Basics.kt
@@ -280,14 +280,14 @@ class Person(firstName: String = "John", lastName: String = "Doe"){
 
 }
 
-fun main(){
-    var myCar = Car()
-    println("brnad is : ${myCar.myBrand}")
-    myCar.maxSpeed = 200
-    println("Maxspped is ${myCar.maxSpeed}")
-    println("Model is ${myCar.myModel}")
-
-}
+//fun main(){
+//    var myCar = Car()
+//    println("brnad is : ${myCar.myBrand}")
+//    myCar.maxSpeed = 200
+//    println("Maxspped is ${myCar.maxSpeed}")
+//    println("Model is ${myCar.myModel}")
+//
+//}
 
 class Car(){
     lateinit var owner: String
@@ -312,4 +312,29 @@ class Car(){
         this.owner = "Frank"
     }
 
+}
+
+data class User(val id : Long, var name: String)
+
+fun main(){
+    val user1 = User(1, "Denis")
+
+//    val name = user1.name
+//    println(name)
+    user1.name = "Michael"
+    val user2 = User(1, "Michael")
+
+    println(user1 == user2)
+
+    println("User Details: $user1")
+
+    val updatedUser = user1.copy(name = "Denis Panjuta")
+    println(user1)
+    println(updatedUser)
+
+    println(updatedUser.component1()) // print 1
+    println(updatedUser.component2()) // prints Denis Panjuta
+
+    val (id,name) = updatedUser
+    println("id=$id, name=$name")
 }
