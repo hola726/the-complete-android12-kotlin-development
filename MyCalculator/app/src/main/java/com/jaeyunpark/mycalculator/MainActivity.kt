fc36056e69b1f786c6a483579d2bf8ba09043721
Park Jae Yun <wodbs720@naver.com>
2022-09-27 22:47:26 +0900

    Add calculate logic

--- MyCalculator/app/src/main/java/com/jaeyunpark/mycalculator/MainActivity.kt
+++ MyCalculator/app/src/main/java/com/jaeyunpark/mycalculator/MainActivity.kt
@@ -70,7 +70,37 @@ class MainActivity : AppCompatActivity() {
                         one = prefix + one
                     }
 
-                    tvInput?.text = (one.toDouble() - two.toDouble()).toString()
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() - two.toDouble()).toString())
+
+                } else if(tvValue.contains("+")){
+                    val splitValue = tvValue.split("+")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() + two.toDouble()).toString())
+
+                }else if(tvValue.contains("*")){
+                    val splitValue = tvValue.split("*")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() * two.toDouble()).toString())
+
+                } else if(tvValue.contains("/")){
+                    val splitValue = tvValue.split("/")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() / two.toDouble()).toString())
 
                 }
 
@@ -81,6 +111,14 @@ class MainActivity : AppCompatActivity() {
 
             }
         }
+    }
+
+    private fun removeZeroAfterDot(result: String): String{
+        var value = result
+        if(result.contains(".0")){
+            value = result.substring(0, result.length-2)
+        }
+        return value
     }
 
     private fun isOperatorAdded(value: String) : Boolean{
