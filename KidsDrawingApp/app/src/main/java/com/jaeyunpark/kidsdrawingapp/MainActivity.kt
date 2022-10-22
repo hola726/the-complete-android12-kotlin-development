3d895b8517812b462ffec369907b2e0a390b3747
Park Jae Yun <wodbs720@naver.com>
2022-10-22 14:41:13 +0900

    Add select brush logic in main activity

--- KidsDrawingApp/app/src/main/java/com/jaeyunpark/kidsdrawingapp/MainActivity.kt
+++ KidsDrawingApp/app/src/main/java/com/jaeyunpark/kidsdrawingapp/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.jaeyunpark.kidsdrawingapp
 
+import android.app.Dialog
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +17,33 @@ class MainActivity : AppCompatActivity() {
         drawingView = findViewById(R.id.drawing_view)
         drawingView?.setSizedForBrush(20.toFloat())
 
+        val ib_brush : ImageButton = findViewById(R.id.ib_brush)
+        ib_brush.setOnClickListener {
+            showBrushSizeChooserDialog()
+        }
+    }
+
+    private fun showBrushSizeChooserDialog(){
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush size: ")
+        val smallBtn : ImageButton = brushDialog.findViewById(R.id.ib_small_brush)
+        smallBtn.setOnClickListener{
+            drawingView?.setSizedForBrush(10.toFloat())
+            brushDialog.dismiss()
+        }
+
+        val mediumButton : ImageButton = brushDialog.findViewById(R.id.ib_medium_brush)
+        mediumButton.setOnClickListener{
+            drawingView?.setSizedForBrush(20.toFloat())
+            brushDialog.dismiss()
+        }
+
+        val largeButton : ImageButton = brushDialog.findViewById(R.id.ib_large_brush)
+        largeButton.setOnClickListener{
+            drawingView?.setSizedForBrush(30.toFloat())
+            brushDialog.dismiss()
+        }
+        brushDialog.show()
     }
 }
