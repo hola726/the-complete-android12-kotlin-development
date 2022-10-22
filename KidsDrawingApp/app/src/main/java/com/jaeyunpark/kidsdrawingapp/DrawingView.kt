d7bf3f3715cb4db6a0aaf8cd4e9039deb4c6600f
Park Jae Yun <wodbs720@naver.com>
2022-10-22 12:53:35 +0900

    Add set brush size in drawing view

--- KidsDrawingApp/app/src/main/java/com/jaeyunpark/kidsdrawingapp/DrawingView.kt
+++ KidsDrawingApp/app/src/main/java/com/jaeyunpark/kidsdrawingapp/DrawingView.kt
@@ -3,6 +3,7 @@ package com.jaeyunpark.kidsdrawingapp
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 
@@ -29,7 +30,6 @@ class DrawingView(context : Context, attrs: AttributeSet) : View(context, attrs)
         mDrawPaint!!.strokeJoin = Paint.Join.ROUND
         mDrawPaint!!.strokeCap = Paint.Cap.ROUND
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
-        mBrushSize = 20.toFloat()
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
@@ -86,9 +86,13 @@ class DrawingView(context : Context, attrs: AttributeSet) : View(context, attrs)
 
         return true
 
+    }
 
-        return super.onTouchEvent(event)
-
+    fun setSizedForBrush(newSize: Float){
+        mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+            newSize, resources.displayMetrics
+        )
+        mDrawPaint!!.strokeWidth = mBrushSize
     }
 
     internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path(){
