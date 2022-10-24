dac09610e39ae1b31829697ba8c7959017f5e7f2
Park Jae Yun <wodbs720@naver.com>
2022-10-24 23:12:37 +0900

    Add on click logic of image button

--- KidsDrawingApp/app/src/main/java/com/jaeyunpark/kidsdrawingapp/MainActivity.kt
+++ KidsDrawingApp/app/src/main/java/com/jaeyunpark/kidsdrawingapp/MainActivity.kt
@@ -6,16 +6,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 
 class MainActivity : AppCompatActivity() {
 
     private var drawingView: DrawingView? = null
+    private var mImageButtonCurrentPaint : ImageButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         drawingView = findViewById(R.id.drawing_view)
         drawingView?.setSizedForBrush(20.toFloat())
+
+        val linearLayOutPaintColor = findViewById<LinearLayout>(R.id.ll_paint_colors)
+
+        mImageButtonCurrentPaint = linearLayOutPaintColor[1] as ImageButton
+        mImageButtonCurrentPaint!!.setImageDrawable(
+            ContextCompat.getDrawable(this,R.drawable.pallet_pressed)
+        )
 
         val ib_brush : ImageButton = findViewById(R.id.ib_brush)
         ib_brush.setOnClickListener {
