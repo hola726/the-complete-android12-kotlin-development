d945ae3c72cbc34b204e685398e4211c9011ed98
Park Jae Yun <wodbs720@naver.com>
2022-11-01 23:17:50 +0900

    Add gallery picker

--- KidsDrawingApp/app/src/main/java/com/jaeyunpark/kidsdrawingapp/MainActivity.kt
+++ KidsDrawingApp/app/src/main/java/com/jaeyunpark/kidsdrawingapp/MainActivity.kt
@@ -3,14 +3,13 @@ package com.jaeyunpark.kidsdrawingapp
 import android.Manifest
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Intent
 import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.LinearLayout
-import android.widget.Toast
+import android.widget.*
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
@@ -22,6 +21,16 @@ class MainActivity : AppCompatActivity() {
 
     private var drawingView: DrawingView? = null
     private var mImageButtonCurrentPaint : ImageButton? = null
+
+    val openGalleryLauncher: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result ->
+            if(result.resultCode == RESULT_OK && result.data!=null){
+                val imageBackGround: ImageView = findViewById(R.id.iv_background)
+
+                imageBackGround.setImageURI(result.data?.data)
+            }
+        }
 
     val requestPermission : ActivityResultLauncher<Array<String>> =
         registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){
@@ -36,6 +45,10 @@ class MainActivity : AppCompatActivity() {
                         "Permission granted now you can read the storage files.",
                         Toast.LENGTH_LONG
                     ).show()
+
+                    val pickIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+
+                    openGalleryLauncher.launch(pickIntent)
                 }else{
                     if(permissionName == Manifest.permission.READ_EXTERNAL_STORAGE){
                         Toast.makeText(this@MainActivity,
