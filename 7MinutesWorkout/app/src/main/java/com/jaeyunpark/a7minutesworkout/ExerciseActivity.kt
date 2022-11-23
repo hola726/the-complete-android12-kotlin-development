ca31a3e2407215e96dc5baa09e093654c061ebf1
Park Jae Yun <wodbs720@naver.com>
2022-11-24 00:07:37 +0900

    feat ( ExerciseActivity ) : ExerciseAcitivity  추가

---
+++ 7MinutesWorkout/app/src/main/java/com/jaeyunpark/a7minutesworkout/ExerciseActivity.kt
@@ -0,0 +1,29 @@
+package com.jaeyunpark.a7minutesworkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jaeyunpark.a7minutesworkout.databinding.ActivityExerciseBinding
+
+class ExerciseActivity : AppCompatActivity() {
+    private var binding : ActivityExerciseBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityExerciseBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        setSupportActionBar(binding?.toolbarExercise)
+
+        if(supportActionBar != null){
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+
+        binding?.toolbarExercise?.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
+
+
+
+    }
+}
