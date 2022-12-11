ee127315768054ac9cdf77cbdc3f79cefcfb4f85
Park Jae Yun <wodbs720@naver.com>
2022-12-11 18:07:17 +0900

    feat( FinishActivity ): 완료화면 로직 추가

---
+++ 7MinutesWorkout/app/src/main/java/com/jaeyunpark/a7minutesworkout/FinishActivity.kt
@@ -0,0 +1,33 @@
+package com.jaeyunpark.a7minutesworkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jaeyunpark.a7minutesworkout.databinding.ActivityFinishBinding
+
+class FinishActivity : AppCompatActivity() {
+    private var binding: ActivityFinishBinding? = null
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        binding = ActivityFinishBinding.inflate(layoutInflater)
+
+        setContentView(binding?.root)
+
+        setSupportActionBar(binding?.toolbarFinishActivity)
+
+        if(supportActionBar != null){
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+
+        binding?.toolbarFinishActivity ?.setNavigationOnClickListener {
+            onBackPressed()
+        }
+        binding?.btnFinish?.setOnClickListener {
+            finish()
+        }
+
+    }
+}
