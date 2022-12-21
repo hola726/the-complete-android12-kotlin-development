2453ba6d00e75aa494863c89d0597ee3072f3e14
Park Jae Yun <wodbs720@naver.com>
2022-12-21 23:59:11 +0900

    feat( HistoryActivity ): 히스토리 리사이클뷰 로직 추가

--- 7MinutesWorkout/app/src/main/java/com/jaeyunpark/a7minutesworkout/HistoryActivity.kt
+++ 7MinutesWorkout/app/src/main/java/com/jaeyunpark/a7minutesworkout/HistoryActivity.kt
@@ -3,7 +3,9 @@ package com.jaeyunpark.a7minutesworkout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.jaeyunpark.a7minutesworkout.databinding.ActivityHistoryBinding
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
@@ -34,8 +36,25 @@ class HistoryActivity : AppCompatActivity() {
     private fun getAllCompletedDates(historyDao: HistoryDao){
         lifecycleScope.launch{
             historyDao.fetchAllDates().collect{ allCompletedDatesList ->
-                for(i in allCompletedDatesList){
-                        Log.e("Date: ",""+i)
+                if(allCompletedDatesList.isNotEmpty()){
+                    binding?.tvHistory?.visibility = View.VISIBLE
+                    binding?.rvHistory?.visibility = View.VISIBLE
+                    binding?.tvNoDataAvailable?.visibility = View.INVISIBLE
+
+                    binding?.rvHistory?.layoutManager = LinearLayoutManager(this@HistoryActivity)
+
+                    val dates = ArrayList<String>()
+                    for(date in allCompletedDatesList){
+                        dates.add(date.date)
+                    }
+                    val historyAdapter = HistoryAdapter(dates)
+
+                    binding?.rvHistory?.adapter = historyAdapter
+
+                }else{
+                    binding?.tvHistory?.visibility = View.GONE
+                    binding?.rvHistory?.visibility = View.GONE
+                    binding?.tvNoDataAvailable?.visibility = View.VISIBLE
                 }
             }
         }
