cfe946637a490a4aa0e17403bccff737c1dabf8e
Park Jae Yun <wodbs720@naver.com>
2022-12-25 17:39:33 +0900

    feat( MainActivity ): 장소추가 플로팅 버튼 핸들링 로직 추가

--- HappyPlaces/app/src/main/java/com/jaeyunpark/happyplaces/MainActivity.kt
+++ HappyPlaces/app/src/main/java/com/jaeyunpark/happyplaces/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.jaeyunpark.happyplaces
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        fabAddHappyPlace.setOnclickListener{
+
+            val intent = Intent(this,AddHappyPlaceActivity::class.java)
+            startActivity(intent)
+
+
+        }
     }
 }
