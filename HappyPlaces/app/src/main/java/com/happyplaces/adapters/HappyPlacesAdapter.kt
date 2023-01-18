785c88166a60ee8f91f4dae4dd6ecf1b647af0a1
Park Jae Yun <wodbs720@naver.com>
2023-01-19 00:03:34 +0900

    feat( HappyPlacesAdapter.kt ): 삭제 핸들 추가

--- HappyPlaces/app/src/main/java/com/happyplaces/adapters/HappyPlacesAdapter.kt
+++ HappyPlaces/app/src/main/java/com/happyplaces/adapters/HappyPlacesAdapter.kt
@@ -11,6 +11,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.happyplaces.R
 import com.happyplaces.activities.AddHappyPlaceActivity
 import com.happyplaces.activities.MainActivity
+import com.happyplaces.database.DatabaseHandler
 import com.happyplaces.models.HappyPlaceModel
 import kotlinx.android.synthetic.main.item_happy_place.view.*
 
@@ -48,6 +49,15 @@ open class HappyPlacesAdapter(
                     onClickListener!!.onClick(position,model)
                 }
             }
+        }
+    }
+
+    fun removeAt(position: Int){
+        val dbHandler = DatabaseHandler(context)
+        val isDeleted = dbHandler.deleteHappyPlace(list[position])
+        if(isDeleted > 0){
+            list.removeAt(position)
+            notifyItemRemoved(position)
         }
     }
 
