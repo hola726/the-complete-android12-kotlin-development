53c645ebb918f3fd82fbd37f4243103f2a7fbba5
Park Jae Yun <wodbs720@naver.com>
2023-01-26 23:17:36 +0900

    feat( MapActivity.kt ): 구글 맵 페이지 activity 추가

---
+++ HappyPlaces/app/src/main/java/com/happyplaces/activities/MapActivity.kt
@@ -0,0 +1,12 @@
+package com.happyplaces.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.happyplaces.R
+
+class MapActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+    }
+}
