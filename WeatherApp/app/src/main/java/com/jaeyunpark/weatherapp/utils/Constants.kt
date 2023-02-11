d0bd1a1cd9e6995eccc8babfcdd6d78ee8591978
Park Jae Yun <wodbs720@naver.com>
2023-02-11 18:03:47 +0900

    feat( Constants.kt ): 날씨 API id 상수 설정

--- WeatherApp/app/src/main/java/com/jaeyunpark/weatherapp/utils/Constants.kt
+++ WeatherApp/app/src/main/java/com/jaeyunpark/weatherapp/utils/Constants.kt
@@ -1,4 +1,4 @@
-package com.weatherapp.utils
+package com.jaeyunpark.weatherapp.utils
 
 import android.content.Context
 import android.net.ConnectivityManager
@@ -7,10 +7,16 @@ import android.os.Build
 
 object Constants {
 
+    const val APP_ID: String = "9f939ef9f159104af9dde450f4548709\n"
+    const val BASE_URL: String = "http://api.openweathermap.org/data/"
+    const val METRIC_UNIT: String = "metric"
+
+
     fun isNetworkAvailable(context: Context): Boolean {
-        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            val network      = connectivityManager.activeNetwork ?: return false
+            val network = connectivityManager.activeNetwork ?: return false
             val activeNetWork = connectivityManager.getNetworkCapabilities(network) ?: return false
             return when {
                 activeNetWork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
